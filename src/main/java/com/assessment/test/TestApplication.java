233674f517e2fb195a84e5a39f968336be352d5f
Yubi48 <yubi48@gmail.com>
2025-10-14 17:51:15 -0400

    testing commit

--- src/main/java/com/assessment/test/TestApplication.java
+++ src/main/java/com/assessment/test/TestApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Application Started");
 		SpringApplication.run(TestApplication.class, args);
+		System.out.println("Application Stopped");
 	}
 
 }
